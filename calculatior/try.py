ffdfc3611e081aa50a0208f7be9ad7309a912e19
thilokasurantha <thilokasurantha91@gmail.com>
2024-01-01 22:17:03 +0530

    hi new update from laptop

--- calculatior/try.py
+++ calculatior/try.py
@@ -1,8 +1,1 @@
-x = "thlioka"
-y =  ["+", "-", "*", "/", "//", "%", "**", "==", "!=", "<", ">", "<=", ">="]
-cur = -1
-while (cur >= len(y)*-1) :
-    print(cur)
-    cur = cur+(-1)
-
-print()
+print("Hello world thiloka")
