cdf57feb58883d980de2463a7a483023aa198c84
thiloka <thilokasurantha91@gmail.com>
2024-01-16 21:11:59 +0530

    bvcbcv

--- chatsystem/chat_app/chat_page/views.py
+++ chatsystem/chat_app/chat_page/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def printname(request) :
+def printname(request) : 
     return render(request, "stranger.html")
